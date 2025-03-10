dbf50e7ae7846a6c8062352cec30759b6a03b7a1
kimdagyeongg <dk32250410@dankook.ac.kr>
2025-03-10 10:56:03 +0900

    첫번째 commit
    Hellow world출력

---
+++ src/Example01.java
@@ -0,0 +1,5 @@
+public class Example01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");//System은 기능담당, out은 화면에 내보낸다는 의미
+    }
+}
---
+++ src/Example02.java
@@ -0,0 +1,6 @@
+public class Example02 {
+    public static void main(String[] args){
+        System.out.println("AA");
+        System.out.println("BB");
+    }
+}
